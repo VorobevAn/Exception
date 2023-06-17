f43c4dff9ed92c368fd93082aba5baa52ed23340
Andrey <cust89@mail.ru>
2023-06-17 23:31:47 +0500

    lincetList

---
+++ Algoritms/src/gbSeminar4/Main.java
@@ -0,0 +1,31 @@
+package gbSeminar4;
+
+public class Main {
+    public static void main(String[] args) {
+        LinList list = new LinList();
+        list.addFirst(3);
+        list.addFirst(2);
+        list.addLast(1);
+        list.print();
+        list.reversal();
+        System.out.println();
+        list.print();
+
+
+
+
+//       LinlistDuo linlistDuo = new LinlistDuo();
+//       linlistDuo.addFirstData(3);
+//       linlistDuo.addFirstData(2);
+//       linlistDuo.addFirstData(1);
+//       linlistDuo.addLast(4);
+//       linlistDuo.addLast(5);
+//       linlistDuo.add(7,3);
+//      // linlistDuo.dellFirst();
+//      // linlistDuo.dellLast();
+//        linlistDuo.print();
+//        System.out.println();
+//       linlistDuo.printRevers();
+
+    }
+}
